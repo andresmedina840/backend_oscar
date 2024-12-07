85b14727f4c1966e7f31fc9aca14473ce6131e51
Carlos Andres Medina <andresmedina840@hotmail.com>
2024-12-06 20:06:20 -0500

    ggggg

---
+++ src/main/java/org/oscarconnect/BackendOscarApplication.java
@@ -0,0 +1,13 @@
+package org.oscarconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOscarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendOscarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/oscarconnect/BackendOscarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oscarconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendOscarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
